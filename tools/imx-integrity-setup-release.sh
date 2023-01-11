5b6b2d7326ca126644a9990e8d94c1cf1d4e6af6
Kshitiz Varshney <kshitiz.varshney@nxp.com>
2023-01-11 11:23:08 +0100

    imx-integrity-setup-release.sh: Updated setup script for langdale
    This commit updates integrity setup script for making it compatible with
    latest yocto version (langdale).
    
    Signed-off-by: Kshitiz Varshney <kshitiz.varshney@nxp.com>

--- tools/imx-integrity-setup-release.sh
+++ tools/imx-integrity-setup-release.sh
@@ -16,3 +16,7 @@ echo "DISTRO_FEATURES:append= \" ima\"" >> $BUILD_DIR/conf/local.conf
 echo "INHERIT += \"ima-evm-rootfs\"" >> $BUILD_DIR/conf/local.conf
 echo "IMA_EVM_BASE = \"\${BSPDIR}/sources/meta-security/meta-integrity\"" >> $BUILD_DIR/conf/local.conf
 echo "IMA_EVM_KEY_DIR = \"\${IMA_EVM_BASE}/data/debug-keys\"" >> $BUILD_DIR/conf/local.conf
+
+echo "LAYERSERIES_COMPAT_parsec-layer = \"kirkstone langdale\"" >> $BUILD_DIR/../sources/meta-security/meta-parsec/conf/layer.conf
+echo "LAYERSERIES_COMPAT_tpm-layer = \"kirkstone langdale\"" >> $BUILD_DIR/../sources/meta-security/meta-tpm/conf/layer.conf
+echo "LAYERSERIES_COMPAT_integrity = \"kirkstone langdale\"" >> $BUILD_DIR/../sources/meta-security/meta-integrity/conf/layer.conf
